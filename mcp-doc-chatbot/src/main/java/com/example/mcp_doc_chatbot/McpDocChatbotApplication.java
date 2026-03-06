377ed71a3e938e529a8b1c378ca2775e89e3248b
Muhammed Fahad T <mhdfhdt@gmail.com>
2026-03-06 16:10:36 +0530

    Initial Commit of spring-ai-mcp-document-agent

---
+++ mcp-doc-chatbot/src/main/java/com/example/mcp_doc_chatbot/McpDocChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcp_doc_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpDocChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpDocChatbotApplication.class, args);
+	}
+
+}
